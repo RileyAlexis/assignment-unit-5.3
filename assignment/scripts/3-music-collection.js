b1d8414e871c1de6dcffb627d4476d79df705aa6
RileyAlexis <MxRileyAlexis@gmail.com>
2023-06-30 14:37:48 -0500

    Added search function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,8 +1,9 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+let tracksArr = [];
 
-function addToCollection(title, artist, yearPublished) {
+function addToCollection(title, artist, yearPublished, tracks) {
     let addedItem = {
         title: title,
         artist: artist,
@@ -12,11 +13,49 @@ function addToCollection(title, artist, yearPublished) {
     return addedItem;
 }
 
-addToCollection('Not a Pretty Girl', 'Ani Difranco', 1995);
-addToCollection('Dilate', 'Ani Difranco', 1996);
-addToCollection('Yes I am', 'Melissa Etheridge', 1993);
-addToCollection('Fearless Love', 'Melissa Etheridge', 2010);
-addToCollection('Feed the Beast', 'Kim Petras', 2023);
-addToCollection('So Jealous', 'Tegan and Sara', 2004);
+console.log(addToCollection('Not a Pretty Girl', 'Ani Difranco', '1995'));
+console.log(addToCollection('Dilate', 'Ani Difranco', '1996'));
+console.log(addToCollection('Yes I am', 'Melissa Etheridge', '1993'));
+console.log(addToCollection('Fearless Love', 'Melissa Etheridge', '2010'));
+console.log(addToCollection('Feed the Beast', 'Kim Petras', '2023'));
+console.log(addToCollection('So Jealous', 'Tegan and Sara', '2004'));
 
-console.table(collection);
+
+function showCollection(displayCollection) {
+    console.log('This collection has', displayCollection.length, ' items');
+    for (i in displayCollection) {
+        console.log(`${displayCollection[i].title} by ${displayCollection[i].artist}, 
+        published in ${displayCollection[i].published}`);
+    }
+}
+
+function findByArtist(artist) {
+    let results = [];
+    let pos = 0;
+    for (i in collection) {
+        if(artist === collection[i].artist) { 
+            results.push(collection[i]); 
+            pos++;
+        }
+    }
+    if(pos>0) return results;
+    else return 'Not Found';
+}
+
+
+showCollection(collection);
+console.table(findByArtist('Ani Difranco'));
+
+function search(criteria) {
+    let result = [];
+    
+  for (i in collection) {
+    if(collection[i].artist === criteria || collection[i].title === criteria 
+        || collection[i].published == criteria) {
+            result.push(collection[i]);
+        }
+  }
+  return result;
+}
+//Allows for a string or number on date search
+console.log('Search', search(2004));
